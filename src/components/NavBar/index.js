8c6db073ab8393241646d309de1a9e4b41acdc17
tabish1235 <tabish554@gmail.com>
2021-10-05 18:05:43 +0500

    ui building

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,22 +1,23 @@
-import { Navbar,NavDropdown,Container,Nav } from "react-bootstrap";
+import { Navbar, NavDropdown, Container, Nav } from "react-bootstrap";
 import "./css/style.css"
-
+import Logo from "../../assets/images/logo.jpg"
 function NavBar() {
     return (
-        <Navbar collapseOnSelect expand="sm"  className="navBar">
+        <Navbar collapseOnSelect expand="sm" className="navBar">
             <Container>
-                <Navbar.Brand href="#home" >React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle className="txt_white" aria-controls="responsive-navbar-nav" />
+                <Navbar.Brand href="#home" className="logo" ><img className="img" src={Logo} alt=""/></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto txt_white">
-                        <Nav.Link className="txt_white" href="#features">Features</Nav.Link>
-                        <Nav.Link className="txt_white"href="#pricing">Pricing</Nav.Link>
-                        
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">Products</Nav.Link>
+                        <Nav.Link href="#pricing">Project Management</Nav.Link>
+                        <Nav.Link href="#pricing">Deals</Nav.Link>
+                        <Nav.Link href="#pricing">Membership</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link className="txt_white" href="#deets">More deets</Nav.Link>
-                        <Nav.Link className="txt_white" eventKey={2} href="#memes">
-                            Dank memes
+                        <Nav.Link href="#deets">Cart</Nav.Link>
+                        <Nav.Link  eventKey={2} href="#memes">
+                            Sign In
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
