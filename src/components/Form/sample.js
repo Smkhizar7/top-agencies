a9f629fbbd4e634ef95f13e8efbebdd9530fbb5a
tabish1235 <tabish554@gmail.com>
2021-10-21 17:37:12 +0500

    code reserve

---
+++ src/components/Form/sample.js
@@ -0,0 +1,47 @@
+{/* <div className="modal_Inputs1 mt-10">
+                            <MyInputText
+                                name="projectName"
+                                type="text"
+                                label="Project Name"
+                                fullWidth={true}
+                                onChange={handleChange}
+                                error={errors.projectName && touched.projectName ? true : false}
+                                helperText={errors.projectName && touched.projectName ? errors.projectName : null}
+                            />
+                        </div>
+                        <div className="modal_Inputs1 mt-10">
+                            <div className="short_Input">
+                                <MyInputText
+                                    type="text"
+                                    label="Plot Size"
+                                    name="plotSize"
+                                    fullWidth={false}
+                                    onChange={handleChange}
+                                    error={errors.plotSize && touched.plotSize ? true : false}
+                                    helperText={errors.plotSize && touched.plotSize ? errors.plotSize : null}
+
+                                />
+                            </div>
+                            <div className="short_Input">
+                                <MyInputText
+                                    type="text"
+                                    label="Quantiy (Bags)"
+                                    name="quantity"
+                                    fullWidth={false}
+                                    onChange={handleChange}
+                                    error={errors.quantity && touched.quantity ? true : false}
+                                    helperText={errors.quantity && touched.quantity ? errors.quantity : null}
+                                />
+                            </div>
+                            <div className="short_Input">
+                                <MyInputText
+                                    type="text"
+                                    label="Rate per Bag"
+                                    name="rate"
+                                    fullWidth={false}
+                                    onChange={handleChange}
+                                    error={errors.rate && touched.rate ? true : false}
+                                    helperText={errors.rate && touched.rate ? errors.rate : null}
+                                />
+                            </div>
+                        </div> */}
